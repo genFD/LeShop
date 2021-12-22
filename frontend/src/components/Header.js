a7cbb99235781463eab2a07b90947b8907ce8661
hermkan <hermann.kanga.dev@gmail.com>
2021-12-22 00:54:47 -0500

    completed navbar

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Le shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='navbarScroll' />
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav
+              className='ml-auto my-2 my-lg-0'
+              style={{ maxHeight: '100px' }}
+              navbarScroll>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i>
+                Sign in
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
